9b37ac4c39dc7e1cb9a7ff836ec6f88883f1880c
Sourabh_kakani <kakanisourabh23@gmail.com>
2024-03-15 00:40:27 +0530

    Day-3 problems

---
+++ 18-printBoolValue.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+void utility(bool a)
+{
+    cout << std::boolalpha;
+    cout << a;
+    // code here
+}
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        bool a;
+        cin >> boolalpha >> a;
+        utility(a);
+        cout << endl;
+    }
+}
+// } Driver Code Ends
