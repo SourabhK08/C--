2da82f50d060f1af989632f8da276189be19bd06
Sourabh_kakani <kakanisourabh23@gmail.com>
2024-03-18 22:03:35 +0530

    function with no arg

---
+++ Functions/2-fnWithNoArg.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+// Function with no argurments
+void helloFunction();
+// Write the helloFunction body below. It should have one statement
+// cout<<"Hello"<<endl;
+void helloFunction()
+{
+    cout << "Hello";
+}
+
+// Write the helloFunction body above. It should have one statement
+// cout<<"Hello"<<endl;
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        helloFunction();
+    }
+}
+// } Driver Code Ends
