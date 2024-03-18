c3e63ebc3bf9e1994f36612355fd93dc4fcd21ca
Sourabh_kakani <kakanisourabh23@gmail.com>
2024-03-18 22:08:50 +0530

    Fn with Arg

---
+++ Functions/3-fnWithArg.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+// Function with arguements
+
+// Write the complete argumentFunction below.
+// The function should take two arguments a and b
+// The function should have only one statement
+
+int argumentFunction(int a, int b)
+{
+    cout << (a + b) << endl;
+}
+
+// Write the complete argumentFunction below.
+// The function should take two arguments a and b
+// The function should have only one statement
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        argumentFunction(a, b);
+    }
+}
+// } Driver Code Ends
